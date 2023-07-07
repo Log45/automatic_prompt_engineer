378de63f8227246c750e5622990d294bb873c19a
Log45 <Skyker604@gmail.com>
2023-07-07 10:55:33 -0400

    eval sampling back on

--- automatic_prompt_engineer/llm.py
+++ automatic_prompt_engineer/llm.py
@@ -219,7 +219,7 @@ class OPT(LLM):
         model = AutoModelForCausalLM.from_pretrained(config['model'],
                                                     device_map='auto', 
                                                     max_length=config['max_tokens'],
-                                                    do_sample=False,
+                                                    do_sample=True,
                                                     top_p=config['top_p'],
                                                     temperature=config['temperature'])
         tokenizer = AutoTokenizer.from_pretrained(config['model'], use_fast=False)
